2ba20628872456e5b43f75f337dcc6de54ce71ff
PiGuy724 <mushroomhinoki@gmail.com>
2024-12-26 20:15:09 -0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./client/index.html", "./client/**/*.{jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
-import viteReact from "@vitejs/plugin-react";
-import viteFastifyReact from "@fastify/react/plugin";
-
-const path = fileURLToPath(import.meta.url);
-
-export default {
-  root: join(dirname(path), "client"),
-  plugins: [viteReact(), viteFastifyReact()],
-  ssr: {
-    external: ["use-sync-external-store"],
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
   },
-};
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
